48b61f3cf0ab42141c3da66f7c3df09fd0c70bff
Mahmoud-1764 <mahmoudchuaibsuliman@gmail.com>
2025-02-06 13:49:00 +0100

    login_pagina_style

--- login.php
+++ login.php
@@ -27,30 +27,18 @@
 <html>
 <head>
    <title>Login Page</title>
-   <style type = "text/css">
-      body {
-         font-family:Arial, Helvetica, sans-serif;
-         font-size:14px;
-      }
-      label {
-         font-weight:bold;
-         width:100px;
-         font-size:14px;
-      }
-      .box {
-         border:#666666 solid 1px;
-      }
-   </style>
+   <link rel="stylesheet" href="Login.css">
+ 
 </head>
-<body bgcolor = "#FFFFFF">
-   <div align = "center">
-      <div style = "width:300px; border: solid 1px #333333; " align = "left">
-         <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Login</b></div>
+<body >
+   <div id="test">
+      <div id="container">
+         <div id="login"><h1>Login</h1></div>
          <div style = "margin:30px">
             <form action = "" method = "post">
-               <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
-               <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-               <input type = "submit" value = " Submit "/><br />
+               <label id="UserName">UserName </label><input id="text_input" type = "text" name = "username" class = "box"/><br /><br />
+               <label id="Password">Password </label><input id="password_input" type = "password" name = "password" class = "box" /><br/><br />
+               <input id="btn" type="submit" value="Submit" /><br />
             </form>
             <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
          </div>
